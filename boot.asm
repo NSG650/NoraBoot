19e0dcac3887a40fd87cc66a21200f544fa87ff6
NSG650 <nsg650@waifu.club>
2022-07-16 15:27:24 +0530

    stage2: in protected mode

--- boot.asm
+++ boot.asm
@@ -1,24 +1,22 @@
 bits 16
 org 0x7c00
 
-xor ax, ax
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-mov ss, ax
-
-mov esp, 0x7C00
-mov ebp, esp
-
 BootEntry:
-    mov di, file
+    mov si, message
     call BootPrint
     call BootEnableA20
+    mov bp, 0x9000 ; set the stack
+    mov sp, bp
+    cli
+    lgdt [BootGdt]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:BootProtectedEntry
     jmp $
 
+
 BootPrint:
-    mov si, di
     mov ah, 0x0e
     loop0:
         lodsb
@@ -36,7 +34,7 @@ BootPrint:
         jmp loop0
     end0:
         ret
-    
+
 BootEnableA20:
 	push ax
 	in al, 0x92
@@ -45,9 +43,71 @@ BootEnableA20:
 	pop ax
 	ret
 
+bits 32
+BootProtectedEntry:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    mov edi, 0xB8000
+    mov ebx, filesize
+    mov ecx, file
+
+    loopA:
+        dec ebx
+        cmp ebx, 0
+        je endA
+        mov eax, [ecx]
+        mov [edi], eax
+        inc edi
+        mov word [edi], 0x1f
+        inc edi
+        inc ecx
+        jmp loopA
+    endA:
+        jmp $
+
 message db "Hello from real mode!", 0xa, 0
 file incbin "file.txt"
-filesize equ $ - file
+filesize equ $ - file + 1
+
+BootGdtStart:
+    dd 0x0 ; 4 byte
+    dd 0x0 ; 4 byte
+
+; GDT for code segment. base = 0x00000000, length = 0xfffff
+BootGdtCode: 
+    dw 0xffff    ; segment length, bits 0-15
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+; GDT for data segment. base and length identical to code segment
+BootGdtData:
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+BootGdtEnd:
+
+; GDT descriptor
+BootGdt:
+    dw BootGdtEnd - BootGdtStart - 1 ; size (16 bit), always one less of its true size
+    dd BootGdtStart ; address (32 bit)
+
+CODE_SEG equ BootGdtCode - BootGdtStart
+DATA_SEG equ BootGdtData - BootGdtStart
 
 times 510-($-$$) db 0
 dw 0xAA55
