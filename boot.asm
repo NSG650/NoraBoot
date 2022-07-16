e67c8ee45272c2692882a83211d14f2cff2eb1dc
NSG650 <nsg650@waifu.club>
2022-07-16 14:49:39 +0530

    stage1: enable a20 and incbin

--- boot.asm
+++ boot.asm
@@ -1,12 +1,23 @@
 bits 16
 org 0x7c00
 
-boot:
-    mov di, message
-    call puts
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+mov ss, ax
+
+mov esp, 0x7C00
+mov ebp, esp
+
+BootEntry:
+    mov di, file
+    call BootPrint
+    call BootEnableA20
     jmp $
 
-puts:
+BootPrint:
     mov si, di
     mov ah, 0x0e
     loop0:
@@ -25,9 +36,18 @@ puts:
         jmp loop0
     end0:
         ret
+    
+BootEnableA20:
+	push ax
+	in al, 0x92
+	or al, 0x02
+	out 0x92, al
+	pop ax
+	ret
 
-message:
-    db "Hello from real mode!", 0xa, 0
+message db "Hello from real mode!", 0xa, 0
+file incbin "file.txt"
+filesize equ $ - file
 
 times 510-($-$$) db 0
 dw 0xAA55
